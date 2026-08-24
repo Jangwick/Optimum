656a4e7ae0b22dfa43a2a419230d088f753c1411
Michael Angelo R. Cantara <cantara.michaelangelo@gmail.com>
2026-08-24 18:10:52 +0800

    chore: add client TypeScript tooling

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,15 +1,17 @@
 import js from '@eslint/js';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
+import tseslint from 'typescript-eslint';
 import globals from 'globals';
 
 export default [
   js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     ignores: ['dist/**', 'node_modules/**', 'coverage/**'],
   },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2023,
       sourceType: 'module',
@@ -28,7 +30,8 @@ export default [
       ...reactHooks.configs.recommended.rules,
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
-      'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
     },
   },
 ];
